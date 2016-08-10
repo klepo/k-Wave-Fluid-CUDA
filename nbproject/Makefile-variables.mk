8e715e85b0b832abcadb9682de57b13a1355aed1
Jiri Jaros <jarosjir@fit.vutbr.cz>
2016-08-10 17:32:54 +0200

    #24: NetBeans IDE project update

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=CUDA-Linux
-CND_ARTIFACT_DIR_Debug=dist/Debug/CUDA-Linux
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=k-wave-fluid-cuda
-CND_ARTIFACT_PATH_Debug=dist/Debug/CUDA-Linux/k-wave-fluid-cuda
-CND_PACKAGE_DIR_Debug=dist/Debug/CUDA-Linux/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/k-wave-fluid-cuda
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=k-wave-fluid-cuda.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/CUDA-Linux/package/k-wave-fluid-cuda.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/k-wave-fluid-cuda.tar
 # Release configuration
 CND_PLATFORM_Release=CUDA-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/CUDA-Linux
